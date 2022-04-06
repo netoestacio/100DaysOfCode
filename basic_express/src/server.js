381b8a403c90073cfb35facfbcf0fb36eac4eb63
Pedro Estacio <pedronetoestacio@gmail.com>
2022-04-06 00:00:27 -0300

    Adicionando mais libs

--- basic_express/src/server.js
+++ basic_express/src/server.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+
 const app = express()
+
+app.use(express.json())
+app.use(logger('dev'))
 
 const port  = 3000
 
-
+const products = [];
 
 app.get("/", (req, res) => {
     res.json({
         message: "Primeira Rota com Node_Express"
     })
+})
+
+app.post("/products", (req, res) => {
+    const body = req.body;
+    console.log(body)
 })
 
 app.listen(port, (req, res) => {
